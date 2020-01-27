3d8e1c64c47a4f9032bc788b6a4a72ba47ac7739
Teodor Chiaburu <teo_chiaburu@yahoo.com>
2020-01-27 19:55:31 +0100

    Add files via upload

--- src/main/resources/Webscrap_IMDB_Library.py
+++ src/main/resources/Webscrap_IMDB_Library.py
@@ -1,6 +1,9 @@
 """ Fortgeschrittene Softwaretechnik """
 """ Teodor Chiaburu, 900526 """
 
+""" Library containing classes for creating the webscraper and testing it """
+
+
 
 # relevant libraries
 import requests
@@ -9,22 +12,40 @@ import unittest
 from numpy import isnan
 from bs4 import BeautifulSoup
 
-
 # lambda function to replace the brackets in a string with an empty string
 replace_brackets = lambda s : s.replace("(", "").replace(")", "")  
 
+""" 
+Class for creating webscraping objects
+
+Attributes: - top_url  = address of website to scrap
+            - html     = the html code of the relevant page(s)
+            - soup     = object of type BeautifulSoup to scrape the data
+            - list_csv = list where the interesting rows of data are to be stored
+            
+Methods:    - get_table() = get the data from the webpages as a table
+            - iterate_films(action) = apply the operation 'action' iteratively on each row of data
+            - add_films(dict_row, row_pop) = adds the relevant features of the films to the list attribute;
+                                             features from row_pop will fist be stored in the dictionary dict_row
+            - convert_to_df() = converts the attribute list_csv into a data frame 
+                                and saves the info as a csv file in the local directory
+"""
 class Webscraper:
     
+    # factory method
     def __init__(self, top_url):
         
         # address of the IMDB page with most popular 100 films
         self.top_url = top_url
+        
         # get html code of the page
         self.html = requests.get(top_url,
                                  headers = {'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
-        # create soup object to parse the html
+        
+        # create BeautifulSoup object to parse the html
         self.soup = BeautifulSoup(self.html.content, "html.parser")
-        # store the rows for the csv in a list
+        
+        # store the rows for the csv file in a list
         self.list_csv = []
                 
     # get table with top films
@@ -36,13 +57,16 @@ class Webscraper:
     # function 'action' passed as argument
     def iterate_films(self, action):
 
+        # initialize data table
         table = self.get_table()
         
+        # each new film is shown on a table row -> tag <tr>
         for row_pop in table.find_all("tr"):
             
-            # every row info will be stored in a dictionary
+            # every row info will be first stored in a dictionary
             dict_row = {}
             
+            # apply operation passed as parameter
             action(dict_row, row_pop)
                             
     # add movie data to the csv list       
@@ -58,7 +82,7 @@ class Webscraper:
         year_and_pop = title_col.find_all("span")
         dict_row["Year"] = replace_brackets(year_and_pop[0].text) 
         
-        # if there is no popularity span, then the popularity stayed the same
+        # if there is no popularity span, then the popularity was constant (0 increase/decrease)
         try:        
             pop = replace_brackets(year_and_pop[1].text)
             pop = int(pop.replace("\n", " ").replace(",", ""))
@@ -90,6 +114,7 @@ class Webscraper:
             dict_row["Rating"] = 0.0
             dict_row["Votes"]  = 0
             
+        # add newly created dictionary to the list
         self.list_csv.append(dict_row)
         
     # converts attribute 'list_csv' to a data frame and saves it locally as a csv
@@ -97,19 +122,34 @@ class Webscraper:
         
         # turn the list into a data frame to save it as csv
         df = pd.DataFrame(self.list_csv)
+        
         # give a name to the id column and start at 1
         df.index.name = "Rank"
         df.index += 1
+        
+        # save csv file
         df.to_csv("imdb_top100.csv")
         
         return df
 
-        
+"""
+Testing class for Webscraper
+Inherited from unittest.TestCase
+
+Attributes: - df = movie data frame
+
+Methods:    - add_dataframe(df) = adds the data frame to be tested to the object
+            - test_shape() = check whther the data frame has the expected number of examples and features
+            - test_isnan() = check if the columns 'Popularity', 'Rating' and 'Votes' have NaNs
+            - test_start_index() = check if the first index in the data frame was correctly set to 1
+"""
 class TestWebscraper(unittest.TestCase):
     
+    # set attribute df to the inputed data frame
     def add_dataframe(self, df):
         self.df = df
 
+    # check the dimensions of the data
     def test_shape(self):
         try: 
             self.assertEqual(self.df.shape, (100, 5))
@@ -118,8 +158,10 @@ class TestWebscraper(unittest.TestCase):
             print('Shape test failed!')
             print(str(err))
 
+    # check whether there are NaNs in the numeric columns
     def test_isnan(self):
         try:
+            # remember the current column name (in case of exception)
             current_col = 'Popularity'
             self.assertFalse(any(isnan(self.df[current_col])))
             current_col = 'Rating'
@@ -131,6 +173,7 @@ class TestWebscraper(unittest.TestCase):
             print('NaN test failed!')
             print(current_col + ' column has NaN values!')
 
+    # check if the starting index was set to 1
     def test_start_index(self):
         try:
             self.assertEqual(self.df.index.start, 1)
--- src/main/resources/dsl_example.py
+++ src/main/resources/dsl_example.py
@@ -1,3 +1,11 @@
+""" Fortgeschrittene Softwaretechnik """
+""" Teodor Chiaburu, 900526 """
+
+""" DSL example: printing a welcoming message for the project """
+
+
+
+# relevant libraries
 import sys
 import importlib
 
@@ -6,8 +14,10 @@ if len(sys.argv) != 2:
     print('usage: %s <name_of_text_file>' % sys.argv[0])
     sys.exit(1)
 
+# add path to the printing function
 sys.path.insert(0, './modules')
 
+# open file containing printing function and apply function
 with open(sys.argv[1], 'r') as file:
     for line in file:
         line = line.strip()
