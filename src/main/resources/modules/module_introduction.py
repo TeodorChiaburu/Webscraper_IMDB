b5ef21db042445e8809e060113cf88f38cebb363
Teodor Chiaburu <teo_chiaburu@yahoo.com>
2020-01-27 20:50:19 +0100

    Update module_introduction.py

--- src/main/resources/modules/module_introduction.py
+++ src/main/resources/modules/module_introduction.py
@@ -4,6 +4,6 @@ def print_introduction(first_name, last_name):
     from datetime import date
     today = date.today()
     print(f"Hello! My name is {first_name} {last_name}.")
-    print(f"You are reading at my software project on {today}.")
+    print(f"You are reading my software project on {today}.")
     print("Enjoy!")
     
